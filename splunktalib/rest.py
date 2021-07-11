daa11792492d2ef111f19f380b1dbda94f5fedfa
Artem Rys <arys@splunk.com>
2021-07-11 14:32:05 +0200

    refactor: remvoe future

--- splunktalib/rest.py
+++ splunktalib/rest.py
@@ -2,9 +2,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-from future import standard_library
-
-standard_library.install_aliases()
 import urllib.request, urllib.parse, urllib.error
 import json
 from traceback import format_exc
